78c521b1dcdfadc4c235ac30a5d502f1fc49d4ab
Краус Евгений Владимирович <evgeniykraus@gmail.com>
2023-02-09 21:59:13 +0700

    Done

---
+++ AUTH/standart_auth/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        UserFactory::new()->count(5)->create();
+    }
+}
